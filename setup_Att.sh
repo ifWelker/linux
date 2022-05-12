bde76aa300ab774d4130bc92fa8295e870606c08
ifWelker <84274394+ifWelker@users.noreply.github.com>
2022-05-12 14:12:22 -0300

    Update setup_Att.sh

--- setup_Att.sh
+++ setup_Att.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+###########################################################################################
+# Arquivo para "instalar o atualizar.sh na pasta bin e realizar a execução sempre quando  #
+#iniciar um bash ou reiniciar o linux.                                                    #
+# Para que seja executado precisa dar permissão de execução:                              #
+# chmod u+x setup_Att.sh                                                                  #
+############################################################################################
+
 echo "Extraindo pacotes de atualizar.sh"
 chmod u+x atualizar.sh
 echo "Criado executavel"
